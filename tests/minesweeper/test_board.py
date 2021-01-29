450e2c175d3fd106ccdc4cf5c59d9b6097620326
Jason Ash <ash.jasont@gmail.com>
2021-01-28 22:22:26 -0800

    update and simplify board tests

--- tests/minesweeper/test_board.py
+++ tests/minesweeper/test_board.py
@@ -9,13 +9,26 @@ from playful.minesweeper.board import Board
 class TestPoint(unittest.TestCase):
     """Test Board class"""
 
+    cells = {
+        Cell(Point(0, 0), value=-1, state="hidden"),
+        Cell(Point(1, 0), value=2, state="revealed"),
+        Cell(Point(0, 1), value=-1, state="flagged"),
+        Cell(Point(1, 1), value=2, state="revealed"),
+    }
+
     def test_bomb_count(self):
-        """Test we correctly identify the number of bombs on a Board"""
-        cells = {
-            Cell(Point(0, 0), value=-1, state="hidden"),
-            Cell(Point(1, 0), value=2, state="hidden"),
-            Cell(Point(2, 0), value=-1, state="hidden"),
-            Cell(Point(3, 0), value=2, state="hidden"),
-            Cell(Point(4, 0), value=-1, state="hidden"),
-        }
-        self.assertEqual(Board(cells).bombs, 3)
+        """Test correctly identifying the number of bombs on a Board"""
+        self.assertEqual(Board(self.cells).bombs, 2)
+
+    def test_height(self):
+        """Test correctly identifying calculate the height of the Board"""
+        self.assertEqual(Board(self.cells).height, 2)
+
+    def test_width(self):
+        """Test correctly identifying calculate the width of the Board"""
+        self.assertEqual(Board(self.cells).width, 2)
+
+    def test_states(self):
+        """Test correctly identifying the states of the Board's Cells"""
+        expected = dict(hidden=1, revealed=2, flagged=1)
+        self.assertEqual(Board(self.cells).states(), expected)
