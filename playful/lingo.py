36560226a3443ffb5144b813922fbbb997f63414
Jason Ash <ash.jasont@gmail.com>
2022-01-15 17:07:22 -0800

    add incorrect_letters function to lingo.py

--- playful/lingo.py
+++ playful/lingo.py
@@ -2,17 +2,19 @@
 Functions to play the word guessing game from the Lingo game show
 https://en.wikipedia.org/wiki/Lingo_(American_game_show)
 """
-from typing import Optional, Tuple
+from typing import Tuple
 
 
-def correct_letters(secret: str, guess: Optional[str] = None) -> Tuple[str, ...]:
+# typing alias for a tuple of single letters
+Letters = Tuple[str, ...]
+
+
+def correct_letters(secret: str, guess: str) -> Letters:
     """
     Return a tuple identifying which letters in the secret word were correctly guessed.
 
     Examples
     --------
-    >>> correct_letters("tacos")
-    ('', '', '', '', '')
     >>> correct_letters("tacos", "tails")
     ('t', 'a', '', '', 's')
     >>> correct_letters("misos", "mosso")
@@ -20,19 +22,15 @@ def correct_letters(secret: str, guess: Optional[str] = None) -> Tuple[str, ...]
     >>> correct_letters("misos", "sassy")
     ('', '', 's', '', '')
     """
-    if guess is None:
-        return tuple("" for _ in secret)
     return tuple(t if t == g else "" for t, g in zip(secret, guess))
 
 
-def misplaced_letters(secret: str, guess: Optional[str] = None) -> Tuple[str, ...]:
+def misplaced_letters(secret: str, guess: str) -> Letters:
     """
     Return a tuple identifying letters that are in the secret word, but misplaced.
 
     Examples
     --------
-    >>> misplaced_letters("tacos")
-    ('', '', '', '', '')
     >>> misplaced_letters("tacos", "teach")
     ('', '', 'a', 'c', '')
     >>> misplaced_letters("misos", "sumps")
@@ -42,9 +40,6 @@ def misplaced_letters(secret: str, guess: Optional[str] = None) -> Tuple[str, ..
     >>> misplaced_letters("misos", "sassy")
     ('s', '', '', '', '')
     """
-    if guess is None:
-        return tuple("" for _ in secret)
-
     # before identifying misplaced letters, we need to account for correct letters.
     # we only want to flag letters in the secret word that aren't in the correct spot.
     correct = correct_letters(secret=secret, guess=guess)
@@ -53,8 +48,32 @@ def misplaced_letters(secret: str, guess: Optional[str] = None) -> Tuple[str, ..
     misplaced = []
     for secret_letter, guess_letter in zip(secret, guess):
         if secret_letter != guess_letter and guess_letter in available:
-            misplaced.append(guess_letter )
+            misplaced.append(guess_letter)
             available.remove(guess_letter)
         else:
             misplaced.append("")
     return tuple(misplaced)
+
+
+def incorrect_letters(secret: str, guess: str) -> Letters:
+    """
+    Return a tuple identifying letters that were guess that are not in the secret word.
+
+    Examples
+    --------
+    >>> incorrect_letters("tangy", "zzzzz")
+    ('z', 'z', 'z', 'z', 'z')
+    >>> incorrect_letters("tacos", "tails")
+    ('', '', 'i', 'l', '')
+    >>> incorrect_letters("misos", "mosso")
+    ('', '', '', '', 'o')
+    >>> incorrect_letters("misos", "sassy")
+    ('', 'a', '', 's', 'y')
+    >>> incorrect_letters()
+    """
+    correct = correct_letters(secret=secret, guess=guess)
+    misplaced = misplaced_letters(secret=secret, guess=guess)
+    return tuple(
+        g if g != s and c == "" and m == "" else ""
+        for s, g, c, m in zip(secret, guess, correct, misplaced)
+    )
--- tests/test_lingo.py
+++ tests/test_lingo.py
@@ -1,7 +1,7 @@
 """Test playful/lingo.py"""
 import unittest
 
-from playful.lingo import correct_letters, misplaced_letters
+from playful.lingo import correct_letters, incorrect_letters, misplaced_letters
 
 
 class TestLingo(unittest.TestCase):
@@ -9,7 +9,6 @@ class TestLingo(unittest.TestCase):
 
     def test_correct_letters(self):
         """Test identifying correct letters, given a secret word and a guess"""
-        self.assertEqual(correct_letters("tacos"), ("", "", "", "", ""))
         self.assertEqual(correct_letters("tacos", "tails"), ("t", "a", "", "", "s"))
         self.assertEqual(correct_letters("misos", "mosso"), ("m", "", "s", "", ""))
         self.assertEqual(correct_letters("misos", "sassy"), ("", "", "s", "", ""))
@@ -17,8 +16,14 @@ class TestLingo(unittest.TestCase):
 
     def test_misplaced_letters(self):
         """Test identifying misplaced letters, given a secret word and a guess"""
-        self.assertEqual(misplaced_letters("tacos"), ("", "", "", "", ""))
         self.assertEqual(misplaced_letters("tacos", "teach"), ("", "", "a", "c", ""))
         self.assertEqual(misplaced_letters("misos", "sumps"), ("s", "", "m", "", ""))
         self.assertEqual(misplaced_letters("misos", "mosso"), ("", "o", "", "s", ""))
         self.assertEqual(misplaced_letters("misos", "sassy"), ("s", "", "", "", ""))
+
+    def test_incorrect_letters(self):
+        """Test identifying misplaced letters, given a secret word and a guess"""
+        self.assertEqual(incorrect_letters("tangy", "zzzzz"), ("z", "z", "z", "z", "z"))
+        self.assertEqual(incorrect_letters("misos", "sumps"), ("", "u", "", "p", ""))
+        self.assertEqual(incorrect_letters("misos", "mosso"), ("", "", "", "", "o"))
+        self.assertEqual(incorrect_letters("misos", "sassy"), ("", "a", "", "s", "y"))
