96537a6813cd65d5a4eae7af0dc44c406a6352d1
Jason Ash <ash.jasont@gmail.com>
2022-01-19 17:07:31 -0800

    build out the scaffolding of functions and tests

--- playful/lingo.py
+++ playful/lingo.py
@@ -5,75 +5,45 @@ https://en.wikipedia.org/wiki/Lingo_(American_game_show)
 from typing import Tuple
 
 
-# typing alias for a tuple of single letters
-Letters = Tuple[str, ...]
-
-
-def correct_letters(secret: str, guess: str) -> Letters:
-    """
-    Return a tuple identifying which letters in the secret word were correctly guessed.
-
-    Examples
-    --------
-    >>> correct_letters("tacos", "tails")
-    ('t', 'a', '', '', 's')
-    >>> correct_letters("misos", "mosso")
-    ('m', '', 's', '', '')
-    >>> correct_letters("misos", "sassy")
-    ('', '', 's', '', '')
-    """
+def correct_letters(secret: str, guess: str) -> Tuple[str, ...]:
+    """Return a tuple of letters where the guess letter equals the secret letter."""
     return tuple(t if t == g else "" for t, g in zip(secret, guess))
 
 
-def misplaced_letters(secret: str, guess: str) -> Letters:
-    """
-    Return a tuple identifying letters that are in the secret word, but misplaced.
-
-    Examples
-    --------
-    >>> misplaced_letters("tacos", "teach")
-    ('', '', 'a', 'c', '')
-    >>> misplaced_letters("misos", "sumps")
-    ('s', '', 'm', '', '')
-    >>> misplaced_letters("misos", "mosso")
-    ('', 'o', '', 's', '')
-    >>> misplaced_letters("misos", "sassy")
-    ('s', '', '', '', '')
-    """
-    # before identifying misplaced letters, we need to account for correct letters.
-    # we only want to flag letters in the secret word that aren't in the correct spot.
-    correct = correct_letters(secret=secret, guess=guess)
-    available = [s for s, c in zip(secret, correct) if c == ""]
-
-    misplaced = []
-    for secret_letter, guess_letter in zip(secret, guess):
-        if secret_letter != guess_letter and guess_letter in available:
-            misplaced.append(guess_letter)
-            available.remove(guess_letter)
-        else:
-            misplaced.append("")
-    return tuple(misplaced)
+def excluded_letters(secret: str, guess: str) -> Tuple[str, ...]:
+    """Return letters that we know aren't included in the secret word."""
+    return ("",)
 
 
-def incorrect_letters(secret: str, guess: str) -> Letters:
-    """
-    Return a tuple identifying letters that were guess that are not in the secret word.
+def misplaced_letters(secret: str, guess: str) -> Tuple[str, ...]:
+    """Return a tuple identifying letters that are in the secret word, but misplaced."""
 
-    Examples
-    --------
-    >>> incorrect_letters("tangy", "zzzzz")
-    ('z', 'z', 'z', 'z', 'z')
-    >>> incorrect_letters("tacos", "tails")
-    ('', '', 'i', 'l', '')
-    >>> incorrect_letters("misos", "mosso")
-    ('', '', '', '', 'o')
-    >>> incorrect_letters("misos", "sassy")
-    ('', 'a', '', 's', 'y')
-    >>> incorrect_letters()
-    """
-    correct = correct_letters(secret=secret, guess=guess)
-    misplaced = misplaced_letters(secret=secret, guess=guess)
-    return tuple(
-        g if g != s and c == "" and m == "" else ""
-        for s, g, c, m in zip(secret, guess, correct, misplaced)
-    )
+
+def has_correct_letters(word: str, letters: Tuple[str, ...]) -> bool:
+    """Return a boolean indicating if a candidate word contains all correct letters."""
+    return all(x == y if y else True for x, y in zip(word, letters))
+
+
+def has_excluded_letters(word: str, letters: Tuple[str, ...]) -> bool:
+    """Return a boolean indicating if a candidate word contains any excluded letters."""
+
+
+def has_misplaced_letters(word: str, letters: Tuple[str, ...]) -> bool:
+    """Return a boolean indicating if a candidate word matches the misplaced letters."""
+
+
+def is_potential_solution(secret: str, guess: str, word: str) -> bool:
+    """Return if a candidate word could be a solution to a secret word, given a guess."""
+    # 1. ensure the candidate word has all the letters we already know about.
+    if not has_correct_letters(word, correct_letters(secret=secret, guess=guess)):
+        return False
+
+    # 2. ensure the word doesn't have any letters we know shouldn't be included.
+    if has_excluded_letters(word, excluded_letters(secret=secret, guess=guess)):
+        return False
+
+    # 3. ensure the word has known, but misplaced, letters.
+    if not has_misplaced_letters(word, misplaced_letters(secret=secret, guess=guess)):
+        return False
+
+    return True
--- tests/test_lingo.py
+++ tests/test_lingo.py
@@ -1,7 +1,15 @@
 """Test playful/lingo.py"""
 import unittest
 
-from playful.lingo import correct_letters, incorrect_letters, misplaced_letters
+from playful.lingo import (
+    correct_letters,
+    excluded_letters,
+    misplaced_letters,
+    has_correct_letters,
+    has_excluded_letters,
+    has_misplaced_letters,
+    is_potential_solution,
+)
 
 
 class TestLingo(unittest.TestCase):
@@ -9,21 +17,103 @@ class TestLingo(unittest.TestCase):
 
     def test_correct_letters(self):
         """Test identifying correct letters, given a secret word and a guess"""
-        self.assertEqual(correct_letters("tacos", "tails"), ("t", "a", "", "", "s"))
-        self.assertEqual(correct_letters("misos", "mosso"), ("m", "", "s", "", ""))
-        self.assertEqual(correct_letters("misos", "sassy"), ("", "", "s", "", ""))
-        self.assertEqual(correct_letters("sorts", "funds"), ("", "", "", "", "s"))
+        cases = [
+            ("tacos", "tails", ("t", "a", "", "", "s")),
+            ("misos", "mosso", ("m", "", "s", "", "")),
+            ("misos", "sassy", ("", "", "s", "", "")),
+            ("sorts", "funds", ("", "", "", "", "s")),
+            ("tangy", "zzzzz", ("", "", "", "", "")),
+        ]
+        for secret, guess, output in cases:
+            with self.subTest(f"testing secret='{secret}' with guess='{guess}'"):
+                self.assertEqual(correct_letters(secret=secret, guess=guess), output)
+
+    def test_excluded_letters(self):
+        """Test identifying single or multiple letters that we know aren't included."""
+        cases = [
+            ("match", "match", ("",)),
+            ("tangy", "zzzzz", ("z",)),
+            ("misos", "sumps", ("p", "u")),
+            ("misos", "mosso", ("oo",)),
+            ("misos", "sassy", ("a", "sss", "y")),
+            ("robed", "worry", ("rr", "w", "y")),
+        ]
+        for secret, guess, output in cases:
+            with self.subTest(f"testing secret='{secret}' with guess='{guess}'"):
+                self.assertEqual(excluded_letters(secret=secret, guess=guess), output)
 
     def test_misplaced_letters(self):
         """Test identifying misplaced letters, given a secret word and a guess"""
-        self.assertEqual(misplaced_letters("tacos", "teach"), ("", "", "a", "c", ""))
-        self.assertEqual(misplaced_letters("misos", "sumps"), ("s", "", "m", "", ""))
-        self.assertEqual(misplaced_letters("misos", "mosso"), ("", "o", "", "s", ""))
-        self.assertEqual(misplaced_letters("misos", "sassy"), ("s", "", "", "", ""))
+        cases = [
+            ("tacos", "teach", ("", "", "a", "c", "")),
+            ("misos", "sumps", ("s", "", "m", "", "")),
+            ("misos", "mosso", ("", "o", "", "s", "")),
+            ("misos", "sassy", ("s", "", "", "", "")),
+            ("robed", "worry", ("", "", "r", "", "")),
+        ]
+        for secret, guess, output in cases:
+            with self.subTest(f"testing secret='{secret}' with guess='{guess}'"):
+                self.assertEqual(misplaced_letters(secret=secret, guess=guess), output)
 
-    def test_incorrect_letters(self):
-        """Test identifying misplaced letters, given a secret word and a guess"""
-        self.assertEqual(incorrect_letters("tangy", "zzzzz"), ("z", "z", "z", "z", "z"))
-        self.assertEqual(incorrect_letters("misos", "sumps"), ("", "u", "", "p", ""))
-        self.assertEqual(incorrect_letters("misos", "mosso"), ("", "", "", "", "o"))
-        self.assertEqual(incorrect_letters("misos", "sassy"), ("", "a", "", "s", "y"))
+    def test_has_correct_letters(self):
+        """Test identifying candidate words that contain all correct letters"""
+        cases = [
+            ("river", ("r", "", "", "", ""), True),
+            ("river", ("r", "", "", "", "r"), True),
+            ("river", ("r", "", "r", "", "r"), False),
+            ("abbot", ("", "b", "", "", ""), True),
+            ("abbot", ("b", "", "", "", ""), False),
+            (
+                "abbot",
+                (
+                    "a",
+                    "b",
+                    "b",
+                    "o",
+                    "t",
+                ),
+                True,
+            ),
+        ]
+        for candidate, exclusions, output in cases:
+            with self.subTest():
+                self.assertEqual(has_correct_letters(candidate, exclusions), output)
+
+    def test_has_excluded_letters(self):
+        """Test identifying candidate words that contain excluded letters"""
+        cases = [
+            ("river", ("r",), True),
+            ("river", ("rr",), True),
+            ("river", ("rrr",), True),
+            ("abbot", ("t",), True),
+            ("abbot", ("b",), True),
+            ("abbot", ("bb",), True),
+            ("abbot", ("bbb",), False),
+        ]
+        for candidate, exclusions, output in cases:
+            with self.subTest():
+                self.assertEqual(has_excluded_letters(candidate, exclusions), output)
+
+    def test_has_misplaced_letters(self):
+        """Test identifying candidate words that contain misplaced letters"""
+        cases = [
+            ("river", ("r",), True),
+            ("river", ("rr",), True),
+            ("river", ("rrr",), True),
+            ("abbot", ("t",), True),
+            ("abbot", ("b",), True),
+            ("abbot", ("bb",), True),
+            ("abbot", ("bbb",), False),
+        ]
+        for candidate, exclusions, output in cases:
+            with self.subTest():
+                self.assertEqual(has_misplaced_letters(candidate, exclusions), output)
+
+    def test_is_potential_solution(self):
+        """Test identifying words that are still potential solutions"""
+        cases = [
+            ("robed", "worry", "river", False),  # ruled out because of second 'r'
+        ]
+        for secret, guess, word, output in cases:
+            with self.subTest():
+                self.assertEqual(is_potential_solution(secret, guess, word), output)
